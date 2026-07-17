2349d1bf9b4c07e57a5060f1484b77077bb9f3aa
josephsbucholtz <joseph.bucholtz@sjsu.edu>
2026-07-17 11:02:57 -0700

    Init

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/home', // Change to your desired landing folder
+        permanent: true,       // Provides a 308 permanent redirect status
+      },
+    ];
+  },
 };
 
 export default nextConfig;
