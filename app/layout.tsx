36c7d713e8eaeddd9b0e975961ee6803b7f65733
josephsbucholtz <joseph.bucholtz@sjsu.edu>
2026-07-17 14:48:27 -0700

    TailWindCss routing Complete
    All is setup for front end design and app logic. Prisma is the only last step.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist } from "next/font/google";
 import { cn } from "@/lib/utils";
+import "./globals.css";
 
 const geist = Geist({subsets:['latin'],variable:'--font-sans'});
 
