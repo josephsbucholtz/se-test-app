84b46a2807e207940f76cb9827c3857c94160423
josephsbucholtz <joseph.bucholtz@sjsu.edu>
2026-07-18 19:02:49 -0700

    Implemented Testing later

--- app/(user)/layout.tsx
+++ app/(user)/layout.tsx
@@ -38,15 +38,6 @@ export default async function UserLayout({
 
                 <NavigationMenuItem>
                     <Link
-                      href="/testing"
-                      className="rounded-md px-4 py-2 text-sm font-medium transition-colors hover:bg-accent hover:text-accent-foreground"
-                    >
-                      Testing
-                    </Link>
-                </NavigationMenuItem>
-
-                <NavigationMenuItem>
-                    <Link
                       href="/developers-notes"
                       className="rounded-md px-4 py-2 text-sm font-medium transition-colors hover:bg-accent hover:text-accent-foreground"
                     >
