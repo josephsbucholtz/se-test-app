a323cf79fdd91afdbd55dd66acc4eb6346c0caf5
josephsbucholtz <joseph.bucholtz@sjsu.edu>
2026-08-03 15:59:42 -0700

    red color for errors

--- app/(user)/testing/page.tsx
+++ app/(user)/testing/page.tsx
@@ -1,10 +1,10 @@
 "use server"
 
-import { getProblem, getRandomProblem } from "./actions";
+import { getRandomProblem } from "./actions";
 import TestingClient from "./testing-client";
 
 export default async function Testing() {
-  const problem = await getProblem();
+
   const randomProblem = await getRandomProblem();
   if (!randomProblem) {
     return (
