dad792d5aff4d926cfa2811213dbb793e5f6aeca
josephsbucholtz <joseph.bucholtz@sjsu.edu>
2026-08-14 17:54:04 -0700

    Adding back testing for Users

--- app/(user)/layout.tsx
+++ app/(user)/layout.tsx
@@ -43,6 +43,16 @@ export default async function UserLayout({
                     </Link>
                 </NavigationMenuItem>
 
+                <NavigationMenuItem>
+                    <Link
+                      href="/testing"
+                      className="rounded-md px-4 py-2 text-sm font-medium transition-colors hover:bg-accent hover:text-accent-foreground"
+                    >
+                    Testing{" "}
+                      <span className="italic text-amber-400">PREMIUM</span>
+                    </Link>
+
+                </NavigationMenuItem>
               </NavigationMenuList>
             </NavigationMenu>
           </div>
